c129e7c6019fd047a40b8c8d390f8bdce299da32
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-10-03 02:02:43 +0300

    Add task joins

--- 05.Subqueries and Joins/02.AddressesWithTowns.sql
+++ 05.Subqueries and Joins/02.AddressesWithTowns.sql
@@ -1,4 +1,7 @@
 SELECT  TOP(50) FirstName,LastName,t.[Name] AS Town,AddressText
-FROM Addresses,Towns t,Employees
-WHERE Addresses.AddressID=Employees.AddressID AND Addresses.TownID=t.TownID
-ORDER BY FirstName,LastName ASC
+FROM Employees AS e
+JOIN Addresses AS a
+ON e.AddressID=a.AddressID
+JOIN Towns AS t
+ON t.TownID=a.TownID
+ORDER BY e.FirstName ASC, e.LastName ASC
---
+++ 05.Subqueries and Joins/03.JoinAndSort.sql
@@ -0,0 +1,5 @@
+SELECT EmployeeID,FirstName,LastName,d.[Name] AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d
+ON e.DepartmentID=d.DepartmentID AND d.[Name] LIKE 'Sales'
+ORDER BY e.EmployeeID ASC
