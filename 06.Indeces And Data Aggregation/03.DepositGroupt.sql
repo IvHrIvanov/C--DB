f1cc4e165b51cccfefb7555b6e0dd4dc90b1ca02
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-10-05 20:29:53 +0300

    Indeces And Aggregations

---
+++ 06.Indeces And Data Aggregation/03.DepositGroupt.sql
@@ -0,0 +1,2 @@
+SELECT DepositGroup, MAX(w.MagicWandSize) AS [LongestMagicWand]  FROM WizzardDeposits AS w
+GROUP BY DepositGroup
---
+++ 06.Indeces And Data Aggregation/04.GroupByDepositGroupAndTotalSum.sql
@@ -0,0 +1,2 @@
+SELECT wd.DepositGroup,SUM(wd.DepositAmount) AS [TotalSum] FROM WizzardDeposits AS wd
+GROUP BY wd.DepositGroup
